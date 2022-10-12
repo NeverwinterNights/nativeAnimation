c5bf8f13128d4cb4d564c968a65a88d4263dc45b
Pavel Kardash <cardash@mail.ru>
2022-10-12 17:18:06 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {useState} from 'react';
+import { Animated, Text, TouchableOpacity, View } from "react-native";
+
+const App = () => {
+  const value = useState(new Animated.ValueXY({x: 0, y: 0}))[0];
+
+  const moveBall = () => {
+    Animated.timing(value, {
+      toValue: {x: 100, y: 100},
+      duration: 1000,
+      useNativeDriver: false,
+    }).start();
+  };
+  return (
+    <View>
+      <Animated.View style={value.getLayout()}>
+        <View
+          style={{
+            width: 100,
+            height: 100,
+            borderRadius: 100 / 2,
+            backgroundColor: 'red',
+          }}
+        />
+      </Animated.View>
+      <TouchableOpacity onPress={moveBall}>
+        <Text>Click me</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });
+
+export default App;
